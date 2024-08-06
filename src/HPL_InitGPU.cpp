c03b0010ca419a7d3ca2b4af39405751fae72fa9
Noel Chalmers <noel.chalmers@gmail.com>
2024-08-06 18:49:53 +0000

    Order GPU selection with local rank ordering

--- src/HPL_InitGPU.cpp
+++ src/HPL_InitGPU.cpp
@@ -39,8 +39,8 @@ void HPL_InitGPU(const HPL_T_grid* GRID) {
 
   MPI_Get_processor_name(host_name, &namelen);
 
-  int localRank = GRID->local_mycol * GRID->local_nprow + GRID->local_myrow;
   int localSize = GRID->local_npcol * GRID->local_nprow;
+  int localRank = rank % localSize;
 
   /* Find out how many GPUs are in the system and their device number */
   int deviceCount;
